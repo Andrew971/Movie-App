a7b8ec2cfecebb39a1b9ec44622458699d9c97da
Andrew971 <sordier.andrew@gmail.com>
2018-02-06 16:18:00 -0800

    update

--- server.js
+++ server.js
@@ -10,46 +10,78 @@ app.get('/', (req, res) => {
 
 
 app.get('/movie/:movieId', (req, res) => {
-  
-  res.send(req.params.movieId);
-});
+  let movie = getMovies();
+  let id = req.params.movieId
+  // res.send(movie[id])
+  res.render('movie', { movie: getMovies(), id })
+})
+
+app.get('/search', (req, res) => {
+  let movies = getMovies();
+  let search = req.query['searchTerm'].toUpperCase()
+  let results= movies.filter((movie) => {
+    return movie.title.toUpperCase().includes(search);
+  });
+
+  res.render('index', { movies:results });
+
+})
+
 
 function getMovies() {
-  return [
-    {
-      title: 'Blade Runner',
-      year: '1982',
-      rated: 'R',
-      released: '25 June 1982',
-      runtime: '1h 57min',
-      genre: 'Sci-Fi, Thriller',
-      director: 'Ridley Scott',
-      writer: 'Hampton Fancher, David Peoples',
-      actors: 'Harrison Ford, Rutger Hauer, Sean Young, Edward James Olmos',
-      plot: 'A blade runner must pursue and try to terminate four replicants who stole a ship in space and have returned to Earth to find their creator.',
-      language: 'English',
-      country: 'USA, Hong Kong',
-      img: '/img/movie-img.jpg'
-    },
-    {
-      title: 'Blade Runner1',
-      year: '1982',
-      rated: 'R',
-      released: '25 June 1982',
-      runtime: '1h 57min',
-      genre: 'Sci-Fi, Thriller',
-      director: 'Ridley Scott',
-      writer: 'Hampton Fancher, David Peoples',
-      actors: 'Harrison Ford, Rutger Hauer, Sean Young, Edward James Olmos',
-      plot: 'A blade runner must pursue and try to terminate four replicants who stole a ship in space and have returned to Earth to find their creator.',
-      language: 'English',
-      country: 'USA, Hong Kong',
-      img: '/img/movie-img.jpg'
-    }
-  ]
-}
+    return [
+      {
+        title: 'Blade Runner',
+        year: '1982',
+        rated: 'R',
+        released: '25 June 1982',
+        runtime: '1h 57min',
+        genre: 'Sci-Fi, Thriller',
+        director: 'Ridley Scott',
+        writer: 'Hampton Fancher, David Peoples',
+        actors: 'Harrison Ford, Rutger Hauer, Sean Young, Edward James Olmos',
+        plot: 'A blade runner must pursue and try to terminate four replicants who stole a ship in space and have returned to Earth to find their creator.',
+        language: 'English',
+        country: 'USA, Hong Kong',
+        img: '/img/movie-img.jpg',
+        id: '0'
+      },
+      {
+        title: 'LoL',
+        year: '1982',
+        rated: 'R',
+        released: '25 June 1982',
+        runtime: '1h 57min',
+        genre: 'Sci-Fi, Thriller',
+        director: 'Ridley Scott',
+        writer: 'Hampton Fancher, David Peoples',
+        actors: 'Harrison Ford, Rutger Hauer, Sean Young, Edward James Olmos',
+        plot: 'A blade runner must pursue and try to terminate four replicants who stole a ship in space and have returned to Earth to find their creator.',
+        language: 'English',
+        country: 'USA, Hong Kong',
+        img: '/img/movie-img.jpg',
+        id: '1'
+      },
+      {
+        title: 'LoL1',
+        year: '1982',
+        rated: 'R',
+        released: '25 June 1982',
+        runtime: '1h 57min',
+        genre: 'Sci-Fi, Thriller',
+        director: 'Ridley Scott',
+        writer: 'Hampton Fancher, David Peoples',
+        actors: 'Harrison Ford, Rutger Hauer, Sean Young, Edward James Olmos',
+        plot: 'A blade runner must pursue and try to terminate four replicants who stole a ship in space and have returned to Earth to find their creator.',
+        language: 'English',
+        country: 'USA, Hong Kong',
+        img: '/img/movie-img.jpg',
+        id: '1'
+      }
+    ]
+  }
 
 app.listen(port, () => {
-  console.log('Server Started on http://localhost:8080');
-  console.log('Press CTRL + C to stop server');
-});
+    console.log('Server Started on http://localhost:8080');
+    console.log('Press CTRL + C to stop server');
+  })
