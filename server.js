dbeae59fdc1ca8a76bdd3ef25c7d2ddf54b2acbb
Andrew971 <sordier.andrew@gmail.com>
2018-02-08 13:30:58 -0800

    update

--- server.js
+++ server.js
@@ -1,50 +1,59 @@
 const express = require('express');
+const request = require('request');
 const app = express();
 const port = process.argv[2] || 8080;
-const fs = require('fs')
-const data = fs.readFileSync('data-movie.json', 'utf8')
-let movies= JSON.parse(data)
 
-function SaveBlogs(){
-  fs.writeFile('data-movie', JSON.stringify(movies), (err)=>{
-      if(err) console.log(err)
-  })
-}
+
+
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-
-app.get('/', (req, res) => {
-  let message = "";
-  res.render('index', { movies: movies, message });
-});
+request('https://api.themoviedb.org/3/discover/movie?api_key=c254f174e3ef7bd559ed74294ff10031&language=en-US', (err, res, body) => {
+  let data = JSON.parse(body)
+  let movies = data.results
 
 
-app.get('/movie/:movieId', (req, res, ) => {
-  let id = req.params.movieId
-  let found = movies.find(function (movie) {
-    return movie.id.toUpperCase().includes(id);
+  app.get('/', (req, res) => {
+    let message = "";
+    res.render('index', { 
+      movies: movies, 
+      message: message });
   })
 
-  res.render('movie', { movie: found, id })
-})
 
-app.get('/search', (req, res) => {
-  let search = req.query['searchTerm'].toUpperCase()
-  let results = movies.filter((movie) => {
-    return movie.title.toUpperCase().includes(search);
-  });
-  function NoMovie() {
+  app.get('/movie/:movieId', (req, res) => {
+    let movieId = req.params.movieId
+      let found = movies.find(function(movie) {
+        if(movie.id == movieId)
+        return movie;
+    })
+   
+    res.render('movie', { movie: found })
+  })
 
-    if (results.length == 0) {
-      return "No movies found"
-    } else {
-      return ""
+  app.get('/search', (req, res) => {
+    let search = req.query['searchTerm'].toUpperCase()
+    let results = movies.filter((movie) => {
+      return movie.title.toUpperCase().includes(search);
+    });
+    function NoMovie() {
+
+      if (results.length == 0) {
+        return "No movies found";
+        res.redirect('/')
+      } else {
+        return ""
+      }
     }
-  }
-  let message = NoMovie()
-  res.render('index', { movies: results, message });
+    let message = NoMovie()
+    res.render('index', { movies: results, 
+      message: message });
+  })
+
+
 })
+
+
 
 app.listen(port, () => {
   console.log('Server Started on http://localhost:8080');
