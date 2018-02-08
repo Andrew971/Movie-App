c29dd1940d1bf7067cc487a113b1db803adafd78
Andrew971 <sordier.andrew@gmail.com>
2018-02-07 21:16:52 -0800

    update

--- server.js
+++ server.js
@@ -1,95 +1,52 @@
 const express = require('express');
 const app = express();
 const port = process.argv[2] || 8080;
+const fs = require('fs')
+const data = fs.readFileSync('data-movie.json', 'utf8')
+let movies= JSON.parse(data)
+
+function SaveBlogs(){
+  fs.writeFile('data-movie', JSON.stringify(movies), (err)=>{
+      if(err) console.log(err)
+  })
+}
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+
 app.get('/', (req, res) => {
-  let message="";
-  res.render('index', { movies: getMovies(), message });
+  let message = "";
+  res.render('index', { movies: movies, message });
 });
 
 
-app.get('/movie/:movieId', (req, res,) => {
-  let movie = getMovies();
+app.get('/movie/:movieId', (req, res, ) => {
   let id = req.params.movieId
+  let found = movies.find(function (movie) {
+    return movie.id.toUpperCase().includes(id);
+  })
 
-  res.render('movie', { movie: getMovies(), id})
+  res.render('movie', { movie: found, id })
 })
 
 app.get('/search', (req, res) => {
-  let movies = getMovies();
   let search = req.query['searchTerm'].toUpperCase()
-  let results= movies.filter((movie) => {
+  let results = movies.filter((movie) => {
     return movie.title.toUpperCase().includes(search);
   });
-  function  NoMovie() {
-    if(results.length == 0){
+  function NoMovie() {
+
+    if (results.length == 0) {
       return "No movies found"
     } else {
       return ""
     }
   }
-let message = NoMovie()
-    res.render('index', { movies:results, message });
+  let message = NoMovie()
+  res.render('index', { movies: results, message });
 })
 
-
-
-function getMovies() {
-    return [
-      {
-        title: 'Blade Runner',
-        year: '1982',
-        rated: 'R',
-        released: '25 June 1982',
-        runtime: '1h 57min',
-        genre: 'Sci-Fi, Thriller',
-        director: 'Ridley Scott',
-        writer: 'Hampton Fancher, David Peoples',
-        actors: 'Harrison Ford, Rutger Hauer, Sean Young, Edward James Olmos',
-        plot: 'A blade runner must pursue and try to terminate four replicants who stole a ship in space and have returned to Earth to find their creator.',
-        language: 'English',
-        country: 'USA, Hong Kong',
-        img: '/img/movie-img.jpg',
-        id: '0'
-      },
-      {
-        title: 'LoL',
-        year: '1982',
-        rated: 'R',
-        released: '25 June 1982',
-        runtime: '1h 57min',
-        genre: 'Sci-Fi, Thriller',
-        director: 'Ridley Scott',
-        writer: 'Hampton Fancher, David Peoples',
-        actors: 'Harrison Ford, Rutger Hauer, Sean Young, Edward James Olmos',
-        plot: 'A blade runner must pursue and try to terminate four replicants who stole a ship in space and have returned to Earth to find their creator.',
-        language: 'English',
-        country: 'USA, Hong Kong',
-        img: '/img/movie-img.jpg',
-        id: '1'
-      },
-      {
-        title: 'LoL1',
-        year: '1982',
-        rated: 'R',
-        released: '25 June 1982',
-        runtime: '1h 57min',
-        genre: 'Sci-Fi, Thriller',
-        director: 'Ridley Scott',
-        writer: 'Hampton Fancher, David Peoples',
-        actors: 'Harrison Ford, Rutger Hauer, Sean Young, Edward James Olmos',
-        plot: 'A blade runner must pursue and try to terminate four replicants who stole a ship in space and have returned to Earth to find their creator.',
-        language: 'English',
-        country: 'USA, Hong Kong',
-        img: '/img/movie-img.jpg',
-        id: '2'
-      }
-    ]
-  }
-
 app.listen(port, () => {
-    console.log('Server Started on http://localhost:8080');
-    console.log('Press CTRL + C to stop server');
-  })
+  console.log('Server Started on http://localhost:8080');
+  console.log('Press CTRL + C to stop server');
+})
